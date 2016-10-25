390753f78b3d1c67e49078df293b4ed096f8b428
Barry Howard <BarryLeeHoward@gmail.com>
2016-10-25 10:34:10 -0400

    resizing issues

--- src/js/main.js
+++ src/js/main.js
@@ -2,42 +2,17 @@ import {OAUTH_KEY} from './OAuth.js';
 import $ from 'jquery';
 import _ from 'lodash';
 
-var base = "https://api.soundcloud.com";
-var clickedBox = null;
-var songNum = 35;
+
+//Initializations
+//-------------------------------------------
+var base = "https://api.soundcloud.com"; //Api url
+var clickedBox = null;  // No box clicked
+var songNum = 35;  // Number of songs displayed
 $(".search-button").click(getResults);
 
 
-function playSong(event){
-	var playBox = event.target;
-	while (!playBox.classList.contains("box")){
-		playBox = playBox.parentElement;
-	}
-	$(clickedBox).toggleClass("chosen-box");
-	$(playBox).toggleClass("chosen-box");
-	clickedBox = playBox;
-
-	var songInfo = playBox.children;
-	var songImage = songInfo[0].getAttribute("src");
-	var songTitle = songInfo[1].innerHTML;
-	var songCount = songInfo[2].innerHTML;
-	var songLength = songInfo[3].innerHTML;
-	var srcURL = songInfo[4].innerHTML;
-	var songDescription = songInfo[5].innerHTML;
-	var songGenre = songInfo[6].innerHTML;
-
-	$(".current-song-img").attr("src", songImage);
-	$(".current-song-title").html(songTitle);
-	$(".current-song-description").html(songDescription);
-	// $(".current-song-genre").html(songGenre);
-	// $(".current-song-count").html("Plays: " + songCount);
-
-
-	var sourceHTML = `<source src=${srcURL} class="audio-source"></source>`;
-	$(".audio-player").html(sourceHTML);
-	$(".audio-player")[0].load();
-}
-
+//Server Query
+//---------------------------------------------
 function getResults(){
 	var term = $(".search-request").val();
 	var data = $.ajax({
@@ -53,50 +28,37 @@ function getResults(){
 	data.then(createGrid);
 }
 
-
-
+//Create Grid of results
+//----------------------------------------
 function createGrid(data){
-	$(".box-container").html("");
-
-	var radios = $(".sort-radio");
+	$(".box-container").html(""); // Reset grid to empty
+	var radios = $(".sort-radio");  //Get sort radio buttons
 	var radio = radios.filter(function(index){
 		if(radios[index].checked===true){return true}else{return false}
-	});
-
-	var sortString = radio.attr("value");
-
-	radios = $(".asc-radio");
+	});  // Find checked radio button
+	var sortString = radio.attr("value");  // Get radio button value
+	radios = $(".asc-radio");  //Get asc/desc radio buttons
 	var radio = radios.filter(function(index){
 		if(radios[index].checked===true){return true}else{return false}
-	});
-
-	var ascString = radio.attr("value");
-
-	var sortedData = sortData(data, sortString, ascString);
-
+	}); //Find checked radio button
+	var ascString = radio.attr("value"); // Get radio button value
+	var sortedData = sortData(data, sortString, ascString);  // Sort data
 	for (var i=0; i<songNum; i++){
 		var boxHTML = songBox(sortedData[i]);
 		$(".box-container").append(boxHTML);
-	}
-	$(".box").click(playSong);
+	} //Populate gird with boxes
+	$(".box").click(playSong);  //Add click even to boxes
 
 }
 
-function sortData (data, sortParam, ascParam){
-	if (sortParam==="relevancy" && ascParam==="asc"){
-		var newData = data.reverse();
-	} else{
-		var newData = _.orderBy(data, sortParam, ascParam);
-	}
-	return newData;
-}
-
-function songBox(song){
-	if (song.artwork_url === null){
+function songBox(song){  //Create Song Box
+	if (song.artwork_url === null){  //Check if song has image
 		var imageHTML = `<img class="list-image" src="https://pedrocadiz13.files.wordpress.com/2014/01/default.png?w=200&h=200">`;
 	} else {
 		var imageHTML = `<img class="list-image" src=${song.artwork_url}>`;
 	}
+
+	//Store parameters of song
 	var titleHTML = `<p class="title">${song.title}</p>`;
 
 	var count = convertPlaybacks(song.playback_count);
@@ -109,9 +71,53 @@ function songBox(song){
 	var description = `<p class="description">Description: ${song.description}</p>`;
 	var genre = `<p class="genre">Genre: ${song.genre}</p>`;
 	var divHTML = `<div class="box">`
-	var HTML = divHTML + imageHTML + titleHTML + countHTML + lengthHTML + srcHTML + description + genre + `</div>`;
+	var HTML = divHTML + imageHTML + titleHTML + countHTML + lengthHTML + srcHTML + description + genre + `</div>`; //Combine HTML
 	return HTML;
 }
+
+function playSong(event){  //Stuff to do when box is clicked
+	var playBox = event.target;
+	while (!playBox.classList.contains("box")){  //Set target to the box rather than the child elements
+		playBox = playBox.parentElement;
+	}
+	$(clickedBox).toggleClass("chosen-box"); //Unshrink previous box
+	$(playBox).toggleClass("chosen-box"); //Grow new box
+	clickedBox = playBox;
+
+	// Pulls values from chosen box
+	var songInfo = playBox.children;  
+	var songImage = songInfo[0].getAttribute("src");
+	var songTitle = songInfo[1].innerHTML;
+	var songCount = songInfo[2].innerHTML;
+	var songLength = songInfo[3].innerHTML;
+	var srcURL = songInfo[4].innerHTML;
+	var songDescription = songInfo[5].innerHTML;
+	var songGenre = songInfo[6].innerHTML;
+
+
+	//Sets player info
+	$(".current-song-img").attr("src", songImage);
+	$(".current-song-title").html(songTitle);
+	$(".current-song-description").html(songDescription);
+
+	//Load new song
+	var sourceHTML = `<source src=${srcURL} class="audio-source"></source>`;
+	$(".audio-player").html(sourceHTML);
+	$(".audio-player")[0].load();
+}
+
+function sortData (data, sortParam, ascParam){
+	if (sortParam==="relevancy" && ascParam==="asc"){
+		var newData = data.reverse();
+	} else{
+		var newData = _.orderBy(data, sortParam, ascParam);
+	}
+	return newData;
+}
+
+
+// Parameter Conversions
+//----------------------------------------------
 
 function timeConvert(milliseconds){
 	var seconds = Math.floor(milliseconds/1000);
